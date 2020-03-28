520b8ec7d46e3ea2a7efa264fcb3715c934e2852
Comick <comick@gmail.org>
2020-03-28 11:21:38 +0100

    Avoid dulication on config.txt

--- board/raspberrypi3/tonio-post-image.sh
+++ board/raspberrypi3/tonio-post-image.sh
@@ -2,15 +2,23 @@
 
 set -e
 
-cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
 
-# enable audio
+if ! grep -qE '^dtparam=audio=' "${BINARIES_DIR}/rpi-firmware/config.txt"; then 
+    echo "Adding 'dtparam=audio=on' to config.txt (enabled audio output)." 
+    cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt" 
+# Enables audio out
 dtparam=audio=on
-
-# enable spidev
-dtparam=spi=on
-
-# enable audio on gpio
-#dtoverlay=pwm-2chan,pin=18,func=2,pin2=13,func2=4
 __EOF__
+fi
 
+
+if ! grep -qE '^dtparam=spi=' "${BINARIES_DIR}/rpi-firmware/config.txt"; then 
+    echo "Adding 'dtparam=spi=' to config.txt (enabled spi interface)." 
+    cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt" 
+# Enables spi for rfid
+dtparam=spi=on
+__EOF__
+fi
+
+# enable audio on gpio, required eg on rpi zer where there is no audio out.
+#dtoverlay=pwm-2chan,pin=18,func=2,pin2=13,func2=4
