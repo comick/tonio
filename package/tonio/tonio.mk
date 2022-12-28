9ef131335ba686112d9524e4c8e21e597272c120
Comick <comick@gmail.com>
2022-12-28 22:52:54 +0100

    build migrated to autotools

--- package/tonio/tonio.mk
+++ package/tonio/tonio.mk
@@ -7,27 +7,16 @@
 TONIO_VERSION = 1.0
 TONIO_SITE = $(BR2_EXTERNAL_TONIO_PATH)/package/tonio/tonio-$(TONIO_VERSION)
 TONIO_SITE_METHOD = local
+TONIO_AUTORECONF = YES
 TONIO_DEPENDENCIES = host-pkgconf vlc alsa-lib libgpiod libmicrohttpd libconfuse
-
-define TONIO_BUILD_CMDS
-    PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig:$(PKG_CONFIG_PATH)" \
-        $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
-endef
-
-define TONIO_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/tonio $(TARGET_DIR)/usr/bin
-    mkdir -p $(TARGET_DIR)/usr/share/tonio/www
-    $(INSTALL) -D -m 0755 $(@D)/www/index.html $(TARGET_DIR)/usr/share/tonio/www/index.html
-    $(INSTALL) -D -m 0755 $(@D)/www/tonio.css $(TARGET_DIR)/usr/share/tonio/www/tonio.css
-    $(INSTALL) -D -m 0755 $(@D)/www/tonio.js $(TARGET_DIR)/usr/share/tonio/www/tonio.js
-endef
 
 define TONIO_INSTALL_INIT_SYSV
     $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_TONIO_PATH)/package/tonio/S15tonio \
         $(TARGET_DIR)/etc/init.d/S15tonio
 endef
 
-$(eval $(generic-package))
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
 
 tonio-deploy: tonio-rebuild
 	ssh root@$(target) '/etc/init.d/S15tonio stop'
