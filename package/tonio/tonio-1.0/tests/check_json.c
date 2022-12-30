3eae949b8cbf2ce3b7ad4e9b97dde2f00d037760
Comick <comick@gmail.com>
2022-12-30 17:17:37 +0100

    fix check?

--- package/tonio/tonio-1.0/tests/check_json.c
+++ package/tonio/tonio-1.0/tests/check_json.c
@@ -46,5 +46,15 @@ Suite * json_suite(void) {
 }
 
 int main(void) {
-    return 0;
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = json_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
