bbe51319a7b5e8f9d7239b255e143127b8b53a0b
Comick <mcdev@playlinux.net>
2026-05-18 21:29:56 +0200

    clean conf and flash scriptz

--- board/raspberrypi/flash-single.sh
+++ board/raspberrypi/flash-single.sh
@@ -5,11 +5,11 @@ PART=$2
 DEV=$3
 ROOT_PART=${DEV}${PART}
 
-echo "Umounting ${ROOT_PART}."
-umount ${ROOT_PART} || true
+echo "Unmounting ${ROOT_PART}."
+umount "${ROOT_PART}" || true
 
-echo "Fashing $ROOTFS_PATH to ${ROOT_PART}"
-sudo dd if=$ROOTFS_PATH of=${ROOT_PART} bs=1M
+echo "Flashing $ROOTFS_PATH to ${ROOT_PART}"
+sudo dd if="$ROOTFS_PATH" of="${ROOT_PART}" bs=1M status=progress
 sync
 
 echo Done.
--- board/raspberrypi/flash.sh
+++ board/raspberrypi/flash.sh
@@ -5,15 +5,25 @@ IMG_PATH=$1
 DEV=$2
 
 echo "Unmounting $DEV and all its partitions."
-ls $DEV?* -1 | xargs umount || true
+ls "$DEV"?* -1 | xargs umount || true
 
 echo "Flashing $IMG_PATH to $DEV."
-sudo dd if=$IMG_PATH of=$DEV
+sudo dd if="$IMG_PATH" of="$DEV" bs=1M status=progress
 sync
 
-echo "Inflating library partition."
-umount ${DEV}3 || true
-sudo parted ${DEV} unit B resizepart 3 $(( `sudo fatresize -i ${DEV}3 | grep -Po '(?<=Max size: )[0-9]+(?=$)'` - 1 ))
-sudo fatresize -s $(( `sudo fatresize -i ${DEV}3 | grep -Po '(?<=Max size: )[0-9]+(?=$)'` - 1 )) ${DEV}3
+echo "Inflating last partition to fill entire SD space left."
+
+# Unmount the last partition
+sudo umount "${DEV}3" || true
+
+# Resize the last partition to fill the entire disk
+sudo parted -s "$DEV" resizepart "3" 100%
+sync
+
+# Wait for kernel to re-read the updated partition table
+sudo partprobe "${DEV}" || sudo blockdev --rereadpt "${DEV}"
+
+# Resize fat file system
+sudo fatresize -s max "${DEV}${LAST_PART}"
 
 echo "Done."
